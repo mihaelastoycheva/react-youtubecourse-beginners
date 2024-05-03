51a592193769148c65ccfe7aeb696d1e74d37a6e
mihaelastoycheva <msdesign.business@gmail.com>
2024-05-03 17:53:29 +0300

    modified App.js with fetch errors to detect

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -7,15 +7,33 @@ import Footer from './Footer';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')) || []);
+  const API_URL = 'http://localhost:3500/items';
 
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
 
 
   useEffect(() => {
-    localStorage.setItem('shoppingList', JSON.stringify(items));
-  }, [items])
+
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive extected data');
+        const listItems = await response.json();
+        console.log(listItems);
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message)
+      }
+    }
+
+    fetchItems()
+    //or
+    //(async () => await fetchItems())();
+  }, [])
 
 
   const addItem = (item) => {
@@ -56,11 +74,16 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <SecContent
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {fetchError && <p style={{ color: "red" }}>
+          {`Error: ${fetchError}`}
+        </p>}
+        {!fetchError && <SecContent
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
