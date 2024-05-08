400c5f851100e03808b9244896e46f875e3aca99
mihaelastoycheva <msdesign.business@gmail.com>
2024-05-08 12:15:40 +0300

    added apiRequest.js

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -5,6 +5,7 @@ import SearchItem from './SearchItem';
 import SecContent from './SecContent';
 import Footer from './Footer';
 import { useState, useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
   const API_URL = 'http://localhost:3500/items';
@@ -42,11 +43,21 @@ function App() {
   }, [])
 
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   }
 
   const handleCheck = (id) => {
